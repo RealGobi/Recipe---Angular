1813b65059e65cb843ea20aac6e5572c61d2839d
Jimmy Svensson <jimmy888swe@gmail.com>
2020-02-07 23:42:16 +0100

    fest: get dummy data working

--- src/app/shoping-list/shoping-list.component.ts
+++ src/app/shoping-list/shoping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Ingredient } from '../shared/ingredient.model';
 @Component({
   selector: 'app-shoping-list',
   templateUrl: './shoping-list.component.html',
   styleUrls: ['./shoping-list.component.css']
 })
 export class ShopingListComponent implements OnInit {
-
+ingredients: Ingredient[] = [
+  new Ingredient('Apples', 5),
+  new Ingredient('Pear', 5)
+];
   constructor() { }
 
   ngOnInit() {
